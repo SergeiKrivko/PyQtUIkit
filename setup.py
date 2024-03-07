2768df22ba4d1242101c919f81f3efa235ba43e7
SergeiKrivko <sergik.krivko@gmail.com>
2024-03-07 14:43:23 +0300

    min python version -> 3.10

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ def readme():
         return f.read()
 
 
+def license():
+    with open('LICENSE', 'r', encoding='utf-8') as f:
+        return f.read()
+
+
 def requirements():
     with open('requirements.txt', 'r', encoding='utf-8') as f:
         return f.read().split()
@@ -21,10 +26,11 @@ if __name__ == '__main__':
         author='SergeiKrivko',
         url='https://github.com/SergeiKrivko',
         long_description=readme(),
+        license=license(),
         version=VERSION,
         package_dir={'PyQtUIkit': 'PyQtUIkit'},
         packages=find_packages(include=['PyQtUIkit*']),
         description='A PyQtUIkit package.',
         install_requires=requirements(),
-        python_requires='>=3.11'
+        python_requires='>=3.10'
     )
